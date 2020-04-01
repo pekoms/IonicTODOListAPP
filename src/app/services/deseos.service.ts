06510c499570516ca619429282c15d337197428b
Pekoms <alejandropi301196@gmail.com>
2020-04-01 21:06:54 +0200

    Style changed to dark + add button tab

---
+++ src/app/services/deseos.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Lista } from '../models/lista.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DeseosService {
+
+  listas: Lista [] = [];
+  constructor() 
+  {
+    const lista1 = new Lista('Recolectar Piedras del Infinito');
+    const lista2 = new Lista('Heroes a Desaparecer');
+
+    this.listas.push(lista1,lista2);
+
+
+   }
+}
